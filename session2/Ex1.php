5234e6060e66908e98ff073acf64b3862cf6cec3
Felixnguyen <ntnam8980@gmail.com>
2017-10-03 19:44:21 +0700

    Update Ex1

--- session2/Ex1.php
+++ session2/Ex1.php
@@ -9,29 +9,29 @@
 		<input type="submit" name="submit">
 </form>
 <?php
-	if ($_SERVER["REQUEST_METHOD"] == "POST"){
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$varA = $_POST['variable'];
 		 if (empty($varA)) {
        		 echo "Text is empty";
-   		 }else{
-   		 	For ($i = 1; $i <= $varA; $i++){
-   		 		if ($i % 3 == 0){
-   		 			echo $i." - ";
-   		 		}
+   		 } else {
+	   		 	for ($i = 1; $i <= $varA; $i++) {
+	   		 		if ($i % 3 == 0){
+	   		 			echo $i."  ";
+	   		 		}
+	   		 	}
+	   		 	echo "<br>";
+	   		 	$x = 0;
+	   		 	while (($varA > 10) && ($x < 1)) {
+	   		 		echo "N lon hon 10 : ".$varA;
+	   		 		$x++;
+	   		 	}
+	   		 	echo "<br>";
+	   		 	$x = 0;
+	   		 	do {
+	   		 		echo "N nho hon hoc bang 15 : ".$varA;
+	   		 		$x++;
+	   		 	} while (($varA <= 15) && ($x < 1));
    		 	}
-   		 	echo "<br>";
-   		 	$x = 0;
-   		 	while (($varA > 10) && ($x < 1)){
-   		 		echo "N lon hon 10 : ".$varA;
-   		 		$x++;
-   		 	}
-   		 	echo "<br>";
-   		 	$x = 0;
-   		 	do{
-   		 		echo "N nho hon hoc bang 15 : ".$varA;
-   		 		$x++;
-   		 	}while (($varA <= 15) && ($x < 1));
-   		 }
 	}
 ?>
 </body>
